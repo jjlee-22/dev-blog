12b091352ebbffc89d65052ca790bc479f4acf53
Jonathan <48492574+jonlee96@users.noreply.github.com>
2019-11-03 14:21:57 -0500

    fixed pagniation and templating issue

--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,16 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
+from django.core.paginator import Paginator
 from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
 	queryset = Post.objects.filter(status=1).order_by('-created_on')
 	template_name = 'blog/index.html'
+	paginate_by = 5
 
 class PostDetail(generic.DetailView):
 	model = Post
 	template_name = 'blog/post_detail.html'
+
