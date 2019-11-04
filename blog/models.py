a3a285b515ee697bb99e418eba6a1ce0a2447dfc
Jonathan <48492574+jonlee96@users.noreply.github.com>
2019-11-04 05:41:28 -0500

    added editable dates on admin site

--- blog/models.py
+++ blog/models.py
@@ -17,9 +17,9 @@ class Post(models.Model):
 	title = models.CharField(max_length=200, unique=True)
 	slug = models.SlugField(max_length=200, unique=True)
 	author = models.ForeignKey(User, on_delete= models.CASCADE, related_name='blog_posts')
-	updated_on = models.DateTimeField(auto_now= True)
+	updated_on = models.DateTimeField('date updated')
 	content = RichTextUploadingField()
-	created_on = models.DateTimeField(auto_now_add=True)
+	created_on = models.DateTimeField('data published')
 	status = models.IntegerField(choices=STATUS, default=0)
 	post_image = models.ImageField(upload_to='img/', default='img/noimage/noimage.jpg', blank=True, null=True)
 
